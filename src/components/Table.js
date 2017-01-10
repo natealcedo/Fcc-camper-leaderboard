2ed347e757146beb36c61bb443b33b8c9729f2c9
Nate Alcedo <ndaljr@gmail.com>
2017-01-10 14:51:53 +0800

    Finished

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,28 @@ import Tablerow from './Tablerow'
 import '../sass/Table.sass'
 
 export default class Table extends Component {
+	constructor() {
+		super();
+		this.state = {
+			allTime: true
+		}
+
+	}
+	sortByTop30(e) {
+		if(this.state.allTimefalse){
+			return;
+		}
+		e.preventDefault();
+		this.setState({
+			allTime: false
+		})
+	}
+	sortByAllTime(e){
+		e.preventDefault();
+		this.setState({
+			allTime: true
+		})
+	}
 	render() {
 		return (
 			<table>
@@ -11,19 +33,38 @@ export default class Table extends Component {
 					<tr>
 						<td className="number">#</td>
 						<td className="camperName">Camper Name</td>
-						<td className="point">Points in past 30 days</td>
-						<td>All time points</td>
+						<td className="point" onClick={this.sortByTop30.bind(this)}><a href="#">Points in past 30 days</a></td>
+						<td onClick={this.sortByAllTime.bind(this)}><a href="#">All time points</a></td>
 					</tr>
 				</thead>
 
 				<tbody>
 					{
-						this.props.top100
-							.map((entry, key) => {
-								return (
-									<Tablerow key={key} number={key + 1} entry={entry} />
-								)
-							})
+						(() => {
+							if (this.state.allTime) {
+								return this.props.top100
+									.sort((a, b) => {
+										return b.alltime - a.alltime
+									})
+									.map((entry, key) => {
+										return (
+											<Tablerow key={key} number={key + 1} entry={entry} />
+										)
+									})
+							}
+							else {
+								return this.props.top100
+									.sort((a, b) => {
+										return b.recent - a.recent
+									})
+									.map((entry, key) => {
+										return (
+											<Tablerow key={key} number={key + 1} entry={entry} />
+										)
+									})
+							}
+
+						})()
 					}
 				</tbody>
 			</table>
--- src/components/Tablerow.js
+++ src/components/Tablerow.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import '../sass/Table.sass'
 export default class Tablerow extends Component {
 	render(){
+		
 		return (
 			<tr className>
 				<td className="number">{this.props.number}</td>
-				<td>{this.props.entry.username}</td>
+				<td><img src={this.props.entry.img}/><a href={'https://freecodecamp.com/' + this.props.entry.username}>{this.props.entry.username}</a></td>
 				<td>{this.props.entry.recent}</td>
 				<td>{this.props.entry.alltime}</td>
 			</tr>
